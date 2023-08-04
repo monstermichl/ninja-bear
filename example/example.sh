30b188c9db76cd9a8af53dd6f071c87699e2a867
monstermichl <vouillarmet.michel@gmail.com>
2023-08-04 22:50:16 +0200

    Fix pushd

--- example/example.sh
+++ example/example.sh
@@ -1,5 +1,5 @@
 # Make sure the script is executed in the example directory even if it's called from somewhere else.
-pushd .
+pushd "${0%/*}" # https://stackoverflow.com/a/207966
 
 OUTPUT_DIR=generated
 
--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Make sure the script is executed in the example directory even if it's called from somewhere else.
-pushd .
+pushd "${0%/*}" # https://stackoverflow.com/a/207966
 
 # Install requirements.
 pip3 install -r requirements.txt
