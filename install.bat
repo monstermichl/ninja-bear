2864170be230304cc569ea1695005e530d23f3ea
monstermichl <vouillarmet.michel@gmail.com>
2023-08-05 11:18:25 +0200

    Remove requirements installation as it's gonna be specified in setup.py

--- install.bat
+++ install.bat
@@ -3,9 +3,6 @@
 rem Make sure the script is executed in the example directory even if it's called from somewhere else.
 pushd %~dp0
 
-rem Install requirements.
-python -m pip install -r requirements.txt
-
 rem Install this module.
 python -m pip install .
 
--- install.sh
+++ install.sh
@@ -1,11 +1,8 @@
 # Make sure the script is executed in the example directory even if it's called from somewhere else.
 pushd "${0%/*}" # https://stackoverflow.com/a/207966
 
-# Install requirements.
-pip3 install -r requirements.txt
-
 # Install this module.
-pip3 install .
+python3 -m pip install .
 
 # Go back to original directory.
 popd
