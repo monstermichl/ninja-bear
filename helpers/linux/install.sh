cc0faf6e31342353e0fb8fdabb8f7b2a6d58bd18
monstermichl <vouillarmet.michel@gmail.com>
2023-08-07 21:34:56 +0200

    Add build step

--- helpers/linux/install.sh
+++ helpers/linux/install.sh
@@ -1,7 +1,10 @@
 # Make sure the script is executed in the example directory even if it's called from somewhere else.
 pushd "${0%/*}/../.." # https://stackoverflow.com/a/207966
 
-# Install this module.
+# Build locally.
+python3 setup.py sdist bdist_wheel
+
+# Install via pip.
 python3 -m pip install .
 
 # Go back to original directory.
