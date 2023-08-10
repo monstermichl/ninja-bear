a648d1055ce3afe4dd65b4e55fcad95489ca1441
monstermichl <vouillarmet.michel@gmail.com>
2023-08-10 22:59:59 +0200

    Add new_language stub files and update README.md files

---
+++ misc/language_support/templates/new_language_config.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from ..generators.<language-generator-file> import <language-generator>  # TODO: Update according to the new language.
+
+from ..base.language_config_naming_conventions import LanguageConfigNamingConventions
+from ..base.language_config import LanguageConfig
+from ..base.name_converter import NamingConventionType
+from ..base.language_type import LanguageType
+from ..base.property import Property
+
+
+class NewLanguageConfig(LanguageConfig):
+    """
+    NewLanguage specific config. For more information about the config methods, refer to LanguageConfig.
+    """
+
+    def __init__(
+        self,
+        config_name: str,
+        properties: List[Property],
+        indent: int = None,
+        naming_conventions: LanguageConfigNamingConventions = None,
+        additional_props = {},
+    ):
+        super().__init__(
+            config_name,
+            LanguageType.<language-type>,  # TODO: Update according to the new language.
+            '<language-file-extension>',  # TODO: Update according to the new language.
+            <lanuage-generator>,  # TODO: Update according to the new language.
+            properties,
+            indent,
+            naming_conventions,
+            additional_props,
+        )
+
+    def _default_naming_convention(self) -> NamingConventionType:
+        return NamingConventionType.<language-default-file-naming-convention>  # TODO: Update according to the new language.
---
+++ misc/language_support/templates/new_language_generator.py
@@ -0,0 +1,30 @@
+from ..base.generator_base import GeneratorBase
+from ..base.property import Property
+from ..base.property_type import PropertyType
+
+
+class NewLanguageGenerator(GeneratorBase):
+    """
+    NewLanguage specific generator. For more information about the generator methods, refer to GeneratorBase.
+    """
+
+    def _property_before_class(self, property: Property) -> str:
+        return ''
+
+    def _property_in_class(self, property: Property) -> str:
+        pass  # TODO: Implement according to the new language.
+
+    def _property_comment(self, comment: str) -> str:
+        pass  # TODO: Implement according to the new language.
+    
+    def _before_class(self, **props) -> str:
+        pass  # TODO: Implement according to the new language.
+
+    def _after_class(self, **props) -> str:
+        pass  # TODO: Implement according to the new language.
+
+    def _start_class(self) -> str:
+        pass  # TODO: Implement according to the new language.
+
+    def _end_class(self) -> str:
+        pass  # TODO: Implement according to the new language.
