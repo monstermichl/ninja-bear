fb7a3e3ffd3d438d112bd82135b0871c1395c6af
monstermichl <vouillarmet.michel@gmail.com>
2023-08-10 23:16:25 +0200

    Add class descriptions

--- src/confluent/base/generator_naming_conventions.py
+++ src/confluent/base/generator_naming_conventions.py
@@ -2,4 +2,7 @@ from .name_converter import NamingConventionType
 
 
 class GeneratorNamingConventions:
+    """
+    Encapsulates the naming conventions which are used by the GeneratorBase class.
+    """
     properties_naming_convention: NamingConventionType = None
--- src/confluent/base/language_config_naming_conventions.py
+++ src/confluent/base/language_config_naming_conventions.py
@@ -3,4 +3,7 @@ from .generator_naming_conventions import GeneratorNamingConventions
 
 
 class LanguageConfigNamingConventions(GeneratorNamingConventions):
+    """
+    Encapsulates the naming conventions which are used by the LanguageConfig class.
+    """
     file_naming_convention: NamingConventionType = None
