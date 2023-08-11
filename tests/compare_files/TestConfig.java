f2be2e5b18fc679933d55ebeda3c6ba635b75e73
monstermichl <vouillarmet.michel@gmail.com>
2023-08-11 16:23:46 +0200

    Update casing

--- tests/compare_files/TestConfig.java
+++ tests/compare_files/TestConfig.java
@@ -2,10 +2,10 @@ package my.test.package;
 
 // Generated with confluent v0.0.1 (https://pypi.org/project/confluent/).
 public class TestConfig {
-    public final static boolean MY_BOOLEAN = true;
-    public final static int MY_INTEGER = 142;
-    public final static float MY_FLOAT = 322.0f;
-    public final static double MY_DOUBLE = 233.9d;
-    public final static String MY_REGEX = "Test Reg(E|e)x"; // Just another RegEx.
-    public final static String MY_SUBSTITUTED_STRING = "Sometimes I just want to scream Hello World!";
+    public final static boolean myBoolean = true;
+    public final static int myInteger = 142;
+    public final static float myFloat = 322.0f;
+    public final static double myDouble = 233.9d;
+    public final static String myRegex = "Test Reg(E|e)x"; // Just another RegEx.
+    public final static String mySubstitutedString = "Sometimes I just want to scream Hello World!";
 }
