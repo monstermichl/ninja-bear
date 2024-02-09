bc7ab5eece784ea62960efa7da1516ab9688850f
monstermichl <vouillarmet.michel@gmail.com>
2024-02-09 17:55:55 +0100

    Update test compare file

--- tests/compare_files/test-config.ts
+++ tests/compare_files/test-config.ts
@@ -1,11 +1,11 @@
-// Generated with confluent v0.2.0 (https://pypi.org/project/confluent/).
-export class TestConfig {
-    public static readonly myBoolean = true;
-    public static readonly myInteger = 142;
-    public static readonly myFloat = 322.0;
-    public static readonly myCombinedFloat = 45724.0;
-    public static readonly myDouble = 233.9;
-    public static readonly myRegex = /Test Reg(E|e)x/; // Just another RegEx.
-    public static readonly mySubstitutedString = 'Sometimes I just want to scream Hello World!';
-    public static readonly myCombinedString = 'I am telling you that this string got included from test-include.yaml.';
-}
+// Generated with confluent v0.3.0 (https://pypi.org/project/confluent/).
+export const TestConfig = {
+    myBoolean: true,
+    myInteger: 142,
+    myFloat: 322.0,
+    myCombinedFloat: 45724.0,
+    myDouble: 233.9,
+    myRegex: /Test Reg(E|e)x/, // Just another RegEx.
+    mySubstitutedString: 'Sometimes I just want to scream Hello World!',
+    myCombinedString: 'I am telling you that this string got included from test-include.yaml.',
+} as const;
