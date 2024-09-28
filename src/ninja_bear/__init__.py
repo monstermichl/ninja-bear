7e216c100db29389b460eeb11dcca7d3fbaa2d32
monstermichl <vouillarmet.michel@gmail.com>
2024-09-28 14:50:45 +0200

    Export LanguageConfigBase and GeneratorBase

--- src/ninja_bear/__init__.py
+++ src/ninja_bear/__init__.py
@@ -1,2 +1,4 @@
 from .base.orchestrator import Orchestrator  # noqa: F401
 from .base.distributor_base import DistributorBase, DistributorCredentials  # noqa: F401
+from .base.language_config_base import LanguageConfigBase  # noqa: F401
+from .base.generator_base import GeneratorBase  # noqa: F401
