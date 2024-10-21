48cc97687f699511632bcfa1c7499dd4f090dd5a
monstermichl <vouillarmet.michel@gmail.com>
2024-10-21 10:25:16 +0200

    Export Plugin and PluginType

--- src/ninja_bear/__init__.py
+++ src/ninja_bear/__init__.py
@@ -6,3 +6,4 @@ from .base.generator_base import GeneratorBase, DumpInfo  # noqa: F401
 from .base.property import Property  # noqa: F401
 from .base.property_type import PropertyType  # noqa: F401
 from .base.name_converter import NameConverter, NamingConventionType  # noqa: F401
+from .base.plugin_manager import Plugin, PluginType  # noqa: F401
