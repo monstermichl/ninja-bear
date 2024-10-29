72e86ccd4306d11bfbc623492a60c5c9c0032358
monstermichl <vouillarmet.michel@gmail.com>
2024-10-29 13:52:52 +0100

    Fix compare string

--- misc/plugins/templates/distributor/test.py
+++ misc/plugins/templates/distributor/test.py
@@ -27,9 +27,7 @@ struct TestConfig:
     float myCombinedFloat = 45724.0
     double myDouble = 233.9
     regex myRegex = /Test Reg(E|e)x/ -- Just another RegEx.
-    string mySubstitutedString = 'Sometimes I just want to scream Hello Mars!'
-    string myCombinedString = 'I am telling you that this string got included from test-include.yaml.'
--- Generated with ninja-bear v1.0.0 (https://pypi.org/project/ninja-bear/).
+    string mySubstitutedString = 'Sometimes I just want to scream Hello World!'
 """
 
 
